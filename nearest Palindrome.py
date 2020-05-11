6a2d9844b7ce00fe0e73d65169f749459794d76c
mugsss <46515811+mugsss@users.noreply.github.com>
2020-05-11 19:50:52 +0530

    Add files via upload

---
+++ nearest Palindrome.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 15:29:24 2020
+
+@author: Mugdha
+"""
+def nearest_palindrome(number):
+    #start writitng your code here
+    while True:
+        sum1 = 0
+        p = number
+        while(p!=0):
+            r= p % 10
+            sum1 = sum1*10 + r
+            p = p//10
+        if(sum1 == number):
+            print(sum1)
+            break
+        else:
+            number = number + 1
+            
+
+number=12331
+nearest_palindrome(number)
+
